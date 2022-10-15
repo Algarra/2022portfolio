7909d5eeedb9c7ab63190ab71fbea8e72be3f845
Daniel Algarra <danielalgarra@Daniels-MBP.fritz.box>
2022-10-15 16:02:58 +0200

    feature: purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,18 @@ module.exports = {
 		'tailwindcss',
 		'postcss',
 		[
+			'postcss-preset-env',
+			{
+				autoprefixer: {
+					flexbox: 'no-2009',
+				},
+				stage: 3,
+				features: {
+					'custom-properties': false,
+				},
+			},
+		],
+		[
 			'@fullhuman/postcss-purgecss',
 			process.env.NODE_ENV === 'production'
 				? {
