e67acb2b9fa847f0e1bab395c2b05fbc53072fd8
Daniel Algarra <danielalgarra@Daniels-MBP.fritz.box>
2022-10-15 15:59:41 +0200

    feature: purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
 module.exports = {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {},
-	},
+	plugins: [
+		'tailwindcss',
+		'postcss',
+		[
+			'@fullhuman/postcss-purgecss',
+			process.env.NODE_ENV === 'production'
+				? {
+						// the paths to all template files
+						content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}', './styles/globals.css'],
+						// function used to extract class names from the templates
+						defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+				  }
+				: false,
+		],
+	],
 }
