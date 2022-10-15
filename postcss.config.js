d13db989cd2705c562bd663116d6fa90264e2302
Daniel Algarra <danielalgarra@Daniels-MBP.fritz.box>
2022-10-15 16:06:11 +0200

    fix: revert purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,29 +1,6 @@
 module.exports = {
-	plugins: [
-		'tailwindcss',
-		'postcss',
-		[
-			'postcss-preset-env',
-			{
-				autoprefixer: {
-					flexbox: 'no-2009',
-				},
-				stage: 3,
-				features: {
-					'custom-properties': false,
-				},
-			},
-		],
-		[
-			'@fullhuman/postcss-purgecss',
-			process.env.NODE_ENV === 'production'
-				? {
-						// the paths to all template files
-						content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}', './styles/globals.css'],
-						// function used to extract class names from the templates
-						defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-				  }
-				: false,
-		],
-	],
+	plugins: {
+		tailwindcss: {},
+		autoprefixer: {},
+	},
 }
