2fd1209c282cdbf9acd69322278ef0b7e9a2559c
Daniel Algarra <danielalgarra@Daniels-MBP.fritz.box>
2022-10-16 20:30:56 +0200

    feat: tests

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,5 @@
 module.exports = {
-	plugins: [
-		'tailwindcss',
-		[
-			'@fullhuman/postcss-purgecss',
-			process.env.NODE_ENV === 'production'
-				? {
-						// the paths to all template files
-						content: [
-							'./pages/**/*.{js,jsx,ts,tsx}',
-							'./components/**/*.{js,jsx,ts,tsx}',
-							'!./pages/**/*.{test.tsx, test.js}',
-							'!./components/**/*.{test.tsx, test.js}',
-						],
-						// function used to extract class names from the templates
-						defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-				  }
-				: false,
-		],
-	],
+	plugins: ['tailwindcss'],
 	webpack: (config, { webpack }) => {
 		config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
 		return config
