22bcbed8aaa2659797bdc437128017c42eb423a9
Daniel Algarra <danielalgarra@Daniels-MBP.fritz.box>
2022-10-19 22:16:07 +0200

    feat: improve SEO + sitemap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,22 +2,17 @@ import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import { DefaultSeo } from 'next-seo'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<Head>
-				<title>Daniel Algarra Navarro - Protfolio page</title>
-				<meta
-					name='description'
-					content="Daniel Algarra portfolio it's made to know more about me, my work experience and my abilities. You will find more about my professional career on the home page and more about me on my room."
-				/>
-				<link rel='canonical' href='https://www.danielalgarranavarro.com/' />
-				<meta name='viewport' content='width=device-width, initial-scale=1' />
-				<meta name='robots' content='index, follow' />
-				<meta charSet='UTF-8' />
-			</Head>
+			<DefaultSeo
+				defaultTitle='Daniel Algarra Navarro - Protfolio page'
+				description="Daniel Algarra portfolio it's made to know more about me, my work experience and my abilities. You will find more about my professional career on the home page and more about me on my room."
+				canonical='https://www.danielalgarranavarro.com/'
+			/>
+
 			<Component {...pageProps} />
 		</>
 	)
--- pages/my-room/index.tsx
+++ pages/my-room/index.tsx
@@ -1,21 +1,14 @@
-import Head from 'next/head'
 import RoomPage from '../../components/MyRoom'
 import { RoomContext } from '../../context/RoomContext'
+import { NextSeo } from 'next-seo'
 
 const Canvas = () => {
 	return (
 		<RoomContext>
-			<Head>
-				<title>My Room -Daniel Algarra Navarro</title>
-				<meta
-					name='description'
-					content="Daniel Algarra my room it's made to know more about me, my hobbies and my personality. You will find more about my professional career on the home page and more about me on my room."
-				/>
-				<link rel='canonical' href='https://www.danielalgarranavarro.com/my-room' />
-				<meta name='viewport' content='width=device-width, initial-scale=1' />
-				<meta name='robots' content='index, follow' />
-				<meta charSet='UTF-8' />
-			</Head>
+			<NextSeo
+				title='My Room -Daniel Algarra Navarro'
+				description="Daniel Algarra my room it's made to know more about me, my hobbies and my personality. You will find more about my professional career on the home page and more about me on my room."
+			/>
 			<RoomPage />
 		</RoomContext>
 	)
