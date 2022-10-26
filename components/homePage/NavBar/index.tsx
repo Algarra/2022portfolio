a79cf55fe1deb49b0b02ceb8d40f0ff125d72d0c
Daniel Algarra <danielalgarra@Daniels-MBP.fritz.box>
2022-10-26 21:11:49 +0200

    feat: update areial-label links

--- components/homePage/NavBar/index.tsx
+++ components/homePage/NavBar/index.tsx
@@ -51,7 +51,7 @@ export const NavBar = () => {
 				}`}
 				id='navbarOne'
 			>
-				<Link href='/my-room'>
+				<Link href='/my-room' aria-atomic>
 					<button className='absolute hidden lg:inline-flex -mt-2 ml-4  items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'>
 						<span className='relative px-5 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
 							VISIT MY ROOM
@@ -133,7 +133,7 @@ export const NavBar = () => {
 						</a>
 					</li>
 				</ul>
-				<Link href='/my-room'>
+				<Link href='/my-room' aria-atomic>
 					<button className='absolute inline-flex lg:hidden -mt-2  items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'>
 						<span className='relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
 							VISIT MY ROOM
