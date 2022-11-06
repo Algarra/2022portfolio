189154014ddb2b5fa4762b3f7df1de36d33041a6
Daniel Algarra <danielalgarra@Daniels-MBP.fritz.box>
2022-11-06 11:25:10 +0100

    feat: meta viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
 			<BankContext actualAccounts={actualAccounts}>
 				<head>
 					<title> Bank aplication</title>
+					<meta name='viewport' content='width=device-width, initial-scale=1' />
 				</head>
 
 				<body className=' bg-black '>
