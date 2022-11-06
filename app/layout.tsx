fcb8fcc5e7605ccb98b370ca3bd3f4c91346c33c
Daniel Algarra <danielalgarra@Daniels-MBP.fritz.box>
2022-11-06 12:05:05 +0100

    feat: improve head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css'
-import 'tailwindcss/tailwind.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import { ReactNode, Suspense } from 'react'
 import { Sidebar } from '../components/bankApp/common/Sidebar'
@@ -16,10 +15,14 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
 	const actualAccounts: accountDetails[] = (await getActualAccounts()).accountsList
 
 	return (
-		<html className=' w-screen h-screen m-0 p-0 pr-0 mr-0 '>
+		<html className=' w-screen h-screen m-0 p-0 pr-0 mr-0 ' lang='en'>
 			<BankContext actualAccounts={actualAccounts}>
 				<head>
 					<title> Bank aplication</title>
+					<meta
+						name='description'
+						content='This is a banck app to test the new features from Next 13, this app is build in an app folder with SSR components.'
+					/>
 					<meta name='viewport' content='width=device-width, initial-scale=1' />
 				</head>
 
