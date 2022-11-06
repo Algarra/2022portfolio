67b6a0d4d40481e07c13a1181bd5c618c36d880e
Daniel Algarra <danielalgarra@Daniels-MBP.fritz.box>
2022-11-06 10:15:32 +0100

    feat: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,6 @@ module.exports = {
 	},
 	plugins: ['react'],
 	rules: {
-		'react/no-unescaped-entities': 'off',
-		'@next/next/no-page-custom-font': 'off',
 		'react-hooks/exhaustive-deps': 'off',
 	},
 }
