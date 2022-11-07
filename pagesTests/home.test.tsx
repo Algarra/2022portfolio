e78c9afab4f0b47728da2c0b60996d133b326c93
Daniel Algarra <danielalgarra@Daniels-MBP.fritz.box>
2022-11-07 21:12:40 +0100

    feat: next13 app bank account management

--- pagesTests/home.test.tsx
+++ pagesTests/home.test.tsx
@@ -1,10 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import Home from '../pages';
+import { fireEvent, render, screen } from '@testing-library/react'
+import Home from '../pages'
 
-test('Render table', async () => {
-  render(<Home countries={[]} currencies={[]} />);
+test('Load + opnen projectInfo', async () => {
+	render(<Home />)
 
-  expect(screen.getAllByText('Loading...')).not.toBeNull();
+	expect(screen.getByText('Welcome to the site to know who I am.')).not.toBeNull()
+	expect(screen.getByText('Recent works')).not.toBeNull()
+	expect(screen.getAllByText('Currently working on')[1]).not.toBeNull()
+	expect(screen.getByText('Technical skills')).not.toBeNull()
+	expect(screen.getByText('Contact me')).not.toBeNull()
 
-  expect(screen.getByText('Create an account')).not.toBeNull();
-});
+	fireEvent.click(screen.getAllByText('Eurosender')[0])
+
+	expect(screen.getByText('Vans tracker')).not.toBeNull()
+})
