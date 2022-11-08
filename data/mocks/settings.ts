c419ccc3c8c64f0caf4cb848a2c7e996157181dd
Daniel Algarra <danielalgarra@Daniels-MBP.fritz.box>
2022-11-08 21:42:36 +0100

    fix: transfers errors

--- data/mocks/settings.ts
+++ data/mocks/settings.ts
@@ -1,30 +1,33 @@
-let ACOUNT_DETAILS_GET_ERROR = false
-let ACOUNT_POST_ERROR = false
-let ACOUNT_PATCH_ERROR = false
-let TRANSFER_POST_ERROR = false
-
 export const getAccountsDetailsGetError = () => {
-	return ACOUNT_DETAILS_GET_ERROR
+	if (!process.env.ACOUNT_DETAILS_GET_ERROR) process.env.ACOUNT_DETAILS_GET_ERROR = JSON.stringify(false)
+
+	return JSON.parse(process.env.ACOUNT_DETAILS_GET_ERROR)
 }
 export const getAccountsPostError = () => {
-	return ACOUNT_POST_ERROR
+	if (!process.env.ACOUNT_POST_ERROR) process.env.ACOUNT_POST_ERROR = JSON.stringify(false)
+
+	return JSON.parse(process.env.ACOUNT_POST_ERROR)
 }
 export const getAccountsPatchError = () => {
-	return ACOUNT_PATCH_ERROR
+	if (!process.env.ACOUNT_PATCH_ERROR) process.env.ACOUNT_PATCH_ERROR = JSON.stringify(false)
+
+	return JSON.parse(process.env.ACOUNT_PATCH_ERROR)
 }
 export const getTransferPostError = () => {
-	return TRANSFER_POST_ERROR
+	if (!process.env.TRANSFER_POST_ERROR) process.env.TRANSFER_POST_ERROR = JSON.stringify(false)
+
+	return JSON.parse(process.env.TRANSFER_POST_ERROR)
 }
 
 export const setAccountsDetailsGetError = (newValue: boolean) => {
-	ACOUNT_DETAILS_GET_ERROR = newValue
+	process.env.ACOUNT_DETAILS_GET_ERROR = JSON.stringify(newValue)
 }
 export const setAccountsPostError = (newValue: boolean) => {
-	ACOUNT_POST_ERROR = newValue
+	process.env.ACOUNT_POST_ERROR = JSON.stringify(newValue)
 }
 export const setAccountsPatchError = (newValue: boolean) => {
-	ACOUNT_PATCH_ERROR = newValue
+	process.env.ACOUNT_PATCH_ERROR = JSON.stringify(newValue)
 }
 export const setTransferPostError = (newValue: boolean) => {
-	TRANSFER_POST_ERROR = newValue
+	process.env.TRANSFER_POST_ERROR = JSON.stringify(newValue)
 }
