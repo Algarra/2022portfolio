14f8a5e3bf56f03387eefc8cac43222a7b9b0723
Daniel Algarra <danielalgarra@Daniels-MacBook-Pro.local>
2022-11-08 14:28:41 +0100

    fix: download

--- components/homePage/NavBar/index.tsx
+++ components/homePage/NavBar/index.tsx
@@ -59,13 +59,13 @@ export const NavBar = () => {
 					</button>
 				</Link>
 
-				{/* <Link href='/bankApp' aria-label='Go to the bank app'>
+				<Link href='/bankApp' aria-label='Go to the bank app'>
 					<button className='fixed hidden left-28 lg:inline-flex -mt-2 ml-10  items-center justify-center p-0.5 mb-2 mr-0 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-lime-800 to-blue-500 group-hover:from-lime-800 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'>
 						<span className='relative px-5 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
 							BANK APP
 						</span>
 					</button>
-				</Link> */}
+				</Link>
 
 				<ul className=' mr-auto lg:justify-end lg:flex text-3xl lg:text-base'>
 					<li
@@ -148,13 +148,13 @@ export const NavBar = () => {
 						</span>
 					</button>
 				</Link>
-				{/* <Link href='/bankApp' aria-label='Go to my bank app'>
-					<button className=' inline-flex lg:hidden mt-2  items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'>
+				<Link href='/bankApp' aria-label='Go to my bank app'>
+					<button className=' inline-flex lg:hidden mt-2  items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-lime-600 to-blue-500 group-hover:from-lime-800 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'>
 						<span className='relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
 							BANK APP
 						</span>
 					</button>
-				</Link> */}
+				</Link>
 			</div>
 
 			<span
