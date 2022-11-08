1917f10e9407581495e2864de36d8a9e6c781140
Daniel Algarra <danielalgarra@Daniels-MBP.fritz.box>
2022-11-08 19:18:57 +0100

    fix: transfers errors

--- data/mocks/transfers.ts
+++ data/mocks/transfers.ts
@@ -1,62 +1,62 @@
 export type transfer = {
-  from?: string | undefined;
-  originAmount?: string | number | undefined;
-  originCurrency?: string | undefined;
-  concept?: string | undefined;
-  to: string;
-  destinationAmount: number;
-  destinationCurrency: string;
-};
+	from?: string | undefined
+	originAmount?: string | number | undefined
+	originCurrency?: string | undefined
+	concept?: string | undefined
+	to: string
+	destinationAmount: number
+	destinationCurrency: string
+}
 
 const transfers: transfer[] = [
-  {
-    from: undefined,
-    originAmount: undefined,
-    originCurrency: undefined,
-    to: 'FR7630006000011234567890189',
-    destinationAmount: 50000,
-    destinationCurrency: 'EUR',
-  },
-  {
-    from: undefined,
-    originAmount: undefined,
-    originCurrency: undefined,
-    to: 'BR1500000000000010932840814P2',
-    destinationAmount: 50000,
-    destinationCurrency: 'USD',
-  },
-  {
-    from: undefined,
-    originAmount: undefined,
-    originCurrency: undefined,
-    to: 'ES7630006000011234567890189',
-    destinationAmount: 50000,
-    destinationCurrency: 'EUR',
-  },
-  {
-    from: undefined,
-    originAmount: undefined,
-    originCurrency: undefined,
-    to: 'PR763000600001123490189',
-    destinationAmount: 50000,
-    destinationCurrency: 'EUR',
-  },
-  {
-    from: undefined,
-    originAmount: undefined,
-    originCurrency: undefined,
-    to: 'GR7630006000011234567890189',
-    destinationAmount: 50000,
-    destinationCurrency: 'EUR',
-  },
-];
+	{
+		from: undefined,
+		originAmount: undefined,
+		originCurrency: undefined,
+		to: 'FR7630006000011234567890189',
+		destinationAmount: 50000,
+		destinationCurrency: 'EUR',
+	},
+	{
+		from: undefined,
+		originAmount: undefined,
+		originCurrency: undefined,
+		to: 'BR150000000000001093284081432',
+		destinationAmount: 50000,
+		destinationCurrency: 'USD',
+	},
+	{
+		from: undefined,
+		originAmount: undefined,
+		originCurrency: undefined,
+		to: 'ES76300060000112345678901893',
+		destinationAmount: 50000,
+		destinationCurrency: 'EUR',
+	},
+	{
+		from: undefined,
+		originAmount: undefined,
+		originCurrency: undefined,
+		to: 'PR76300060022001123490189',
+		destinationAmount: 50000,
+		destinationCurrency: 'EUR',
+	},
+	{
+		from: undefined,
+		originAmount: undefined,
+		originCurrency: undefined,
+		to: 'GR763000600330011234567890189',
+		destinationAmount: 50000,
+		destinationCurrency: 'EUR',
+	},
+]
 
 export const setTransfers = (newTransfers: transfer[]) => {
-  process.env.TRANSFERS = JSON.stringify([...newTransfers]);
-};
+	process.env.TRANSFERS = JSON.stringify([...newTransfers])
+}
 
 export const getTransfers = () => {
-  if (!process.env.TRANSFERS) process.env.TRANSFERS = JSON.stringify([...transfers]);
+	if (!process.env.TRANSFERS) process.env.TRANSFERS = JSON.stringify([...transfers])
 
-  return JSON.parse(process.env.TRANSFERS);
-};
+	return JSON.parse(process.env.TRANSFERS)
+}
